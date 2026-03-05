07a2e5b77b3d2db7d90472c26e91a036d82dd4c2
Aryan <katocharyan743@gmail.com>
2026-03-05 21:57:17 +0530

    Mathfordsa : Seive of Eratosthenes

---
+++ com/dsa/mathsfordsa/Seive.java
@@ -0,0 +1,30 @@
+package com.dsa.mathsfordsa;
+/*problem : check for prime number in a range of n.
+  approach : -> Assume all numbers from 2 to n are prime.
+             -> start from first prime number 2.
+             -> mark all multiples of its as not prime.
+             -> check for next unmarked num and repeat.
+             -> continue this process till root n.
+time complexity : O(n * log(log(n))) and space complexity : O(n) {for extra space}           
+*/
+public class Seive {
+    public static void main(String[] args) {
+        int n = 40;
+        boolean[] primes = new boolean[n+1];
+        seive(primes,n);
+    }
+    static void seive(boolean[] primes,int n){
+        for(int i = 2;i*i <= n; i++){
+            if(!primes[i]){//for false
+                for(int j = i*2;j <= n; j+=i){
+                    primes[j] = true;
+                }
+            }
+        }
+        for(int i = 2; i <= n;i++){
+            if(!primes[i]){
+                System.out.println(i + " ");
+            }
+        }
+    }
+}

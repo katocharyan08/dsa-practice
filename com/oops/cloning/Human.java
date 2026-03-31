f3ceb62b7e7a02769120f2a1109ead4d8959472c
Aryan <katocharyan743@gmail.com>
2026-03-31 22:15:23 +0530

    cloning file added

--- com/oops/cloning/Human.java
+++ com/oops/cloning/Human.java
@@ -3,13 +3,16 @@ package com.oops.cloning;
 public class Human implements Cloneable{
     int age;
     String name;
+    int[] arr;
     Human(){
         this.age = 23;
         this.name = "Aryan";
+        
     }
     Human(int age ,String name){
         this.age = age;
         this.name = name;
+        this.arr = new int[]{1,2,3,4};
     }
     Human(Human other){
         this.age = other.age;
@@ -17,6 +20,12 @@ public class Human implements Cloneable{
     }
     @Override
     protected Object clone() throws CloneNotSupportedException{
-        return super.clone();
+        Human h3 = (Human)super.clone();//shallow copy
+        //deep copy
+        h3.arr = new int[h3.arr.length];
+        for(int i = 0;i < h3.arr.length;i++){
+            h3.arr[i] = this.arr[i];
+        }
+        return h3;
     }
 }
--- com/oops/cloning/Main.java
+++ com/oops/cloning/Main.java
@@ -1,5 +1,7 @@
 package com.oops.cloning;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
         Human h1 = new Human(32,"Aryan");
@@ -7,5 +9,9 @@ public class Main {
 
         Human h3 = (Human)h1.clone();
         System.out.println(h3.age +" , "+ h3.name);
+        System.out.println(Arrays.toString(h3.arr));
+        h3.arr[0] = 11;
+        System.out.println(Arrays.toString(h1.arr));
+
     }
 }

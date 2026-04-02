e2e0662066f93b2ff0a0898bcbcec4b3c922d7a9
Aryan <katocharyan743@gmail.com>
2026-04-02 20:16:32 +0530

    For loop pattern

---
+++ com/dsa/pattern/RightTriNum.java
@@ -0,0 +1,10 @@
+package com.dsa.pattern;
+
+public class RightTriNum {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++){
+           
+            System.out.println();
+        }
+    }
+}

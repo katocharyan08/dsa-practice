63b4ba6d35e2f32f1c7d58cf6e8de42f678c209f
Aryan <katocharyan743@gmail.com>
2026-04-27 07:39:06 +0530

    Linear search modified

--- com/dsa/recursion/LinearSearch.java
+++ com/dsa/recursion/LinearSearch.java
@@ -2,14 +2,15 @@ package com.dsa.recursion;
 
 import java.util.ArrayList;
 
-public class LiearSearch {
+public class LinearSearch {
     public static void main(String[] args) {
         int[] arr = {3,2,1,18,18,9};
         System.out.println(findFromFirst(arr, 18, 0));
         System.out.println(findFromLast(arr, 18, arr.length-1));
         System.out.println(find(arr, 18, 0));
-        findAllIndex(arr,18, 0);
-         System.out.println(list);
+        //findAllIndex(arr,18, 0);
+        // System.out.println(list);
+        System.out.println(findAllIndex(arr, 18, 0, new ArrayList<>()));
     }
     static int findFromFirst(int[] arr, int target,int i){
         if(arr.length == i){
@@ -44,5 +45,14 @@ public class LiearSearch {
                 list.add(i);
             }
             findAllIndex(arr, target, i+1);
+        }
+          static ArrayList<Integer> findAllIndex(int[] arr, int target,int i,ArrayList<Integer> list){
+            if(arr.length == i){
+                return list;
+            }
+            if(arr[i] == target){
+                list.add(i);
+            }
+            return findAllIndex(arr, target, i+1,list);
         }
 }

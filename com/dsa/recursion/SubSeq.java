58a04de869aff5d3635d293a3412b6e1d8fbf56d
Aryan <katocharyan743@gmail.com>
2026-08-26 21:09:07 +0530

    Adding Arraylist in functions

--- com/dsa/recursion/SubSeq.java
+++ com/dsa/recursion/SubSeq.java
@@ -1,8 +1,10 @@
 package com.dsa.recursion;
 
+import java.util.ArrayList;
+
 public class SubSeq {
-    public static void main(String[] args) {
-        subSeq("","abc");
+    public static void main(String[] args) {  
+        System.out.println(subSeq2("","abc"));
     }
     static void subSeq(String processed, String un_processed){
         //basecase 
@@ -14,5 +16,32 @@ public class SubSeq {
 
         subSeq(processed + ch, un_processed.substring(1));
         subSeq(processed, un_processed.substring(1));
+    }
+    //pass in argument
+    static ArrayList<String> subSeq(String p ,String up ,ArrayList<String> list){
+        if(up.isEmpty()){
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        subSeq(p + ch, up.substring(1), list);
+        subSeq(p, up.substring(1), list);
+
+        return list;
+    }
+    //pass inside function
+    static ArrayList<String> subSeq2(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+
+        ArrayList<String> left = subSeq2(p + ch, up.substring(1));
+        ArrayList<String> right = subSeq2(p, up.substring(1));
+
+        left.addAll(right);
+        return left; 
     }
 }

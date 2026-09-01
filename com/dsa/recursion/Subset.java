fa63ef428cf841326563b7f2606976e6a17bb68f
Aryan <katocharyan743@gmail.com>
2026-09-01 21:17:35 +0530

    Subset problem by recursion

--- com/dsa/recursion/Subset.java
+++ com/dsa/recursion/Subset.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 
 public class Subset {
     public static void main(String[] args) {
-        int[] arr = {1,2,3};
-        List<List<Integer>> ans = subSet(arr);
-        for(List<Integer> list : ans){
-            System.out.println(list);
-        }
+        // int[] arr = {1,2,3};
+        // List<List<Integer>> ans = subSet(arr);
+        // for(List<Integer> list : ans){
+        //     System.out.println(list);
+        // }
+        int[] nums = {1,2,3};
+        List<List<Integer>> ans = subSets(nums);
+        System.out.println(ans);
     }
+    //iteration
     static List<List<Integer>> subSet(int[] arr){
     List<List<Integer>> outer = new ArrayList<>();
 
@@ -24,6 +28,27 @@ public class Subset {
         }
     }
     return outer;
+    }
+    //recursion
+    static List<List<Integer>> subSets(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        backTrack(result,nums,new ArrayList<>(),0);
+        return result;       
+    }
+    static void backTrack(List<List<Integer>> outer,int[] nums,List<Integer> inner,int start){
+        //including empty list at starting
+        outer.add(new ArrayList<>(inner));
+
+        for(int i = start; i < nums.length; i++){
+            //take
+            inner.add(nums[i]);
+
+            //backtrack
+            backTrack(outer, nums, inner, i + 1);
+
+            //remove
+            inner.remove(inner.size() - 1);
+        }
     }
 }
 
